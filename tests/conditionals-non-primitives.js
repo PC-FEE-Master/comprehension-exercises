a91ed84a8288418ef2c6593aa269e6da4adf0850
thomas1117 <thomasshannon117@yahoo.com>
2020-08-06 16:16:52 -0700

    fix conditionals

--- tests/conditionals-non-primitives.js
+++ tests/conditionals-non-primitives.js
@@ -2,6 +2,7 @@ describe('should be able to compare object values', () => {
     let arr = []
     let isEmpty
     // if statement here
+  
     let isNotEmpty
     // if statement here
     it('isEmpty should be true', () => {
@@ -13,25 +14,27 @@ describe('should be able to compare object values', () => {
     let arr2 = [1]
     let notEmpty
     // if statement here
-    it('notEmpty should be false', () => {
+    it('isNotEmpty should be false', () => {
         expect(notEmpty).toEqual(true)
     })
-
+  
     let arr3 = [1,2,3,4]
     let containsAtLeast2
     it('containsAtLeast2 should be true', () => {
         expect(containsAtLeast2).toEqual(true)
     })
-
+  
     const obj = {
         name: 'John',
         age: 21
     }
     let isJohn
     let olderThan20
-    // if statements here
+    // if statement here
     it('isJohn should be true', () => {
         expect(isJohn).toEqual(true)
-        expect(olderThan20).toEqual(true)
     })
-})
+    it('olderThan20 should be true', () => {
+      expect(olderThan20).toEqual(true)
+    })
+  })
