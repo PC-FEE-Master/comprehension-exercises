48d215a104d7af1947f959a2f0db8dd5f9ae5c43
thomas1117 <thomasshannon117@yahoo.com>
2020-08-06 15:26:50 -0700

    - fix unit test for non=primitives

--- solutions/data-access-non-primitives.js
+++ solutions/data-access-non-primitives.js
@@ -9,12 +9,20 @@ describe('should be able to get/delete/modify values from objects', () => {
     let last = person.last
     let full = person.first + ' ' + person.last
     delete person.ssn
-    person.age = 23
-    it('should access person properly', () => {
+    person.age++
+    it('should access persons first name', () => {
         expect(first).toEqual('John')
+    })
+    it('should access persons last name', () => {
         expect(last).toEqual('Smith')
+    })
+    it('should access persons full name', () => {
         expect(full).toEqual('John Smith')
+    })
+    it('should not have a ssn', () => {
         expect(person.ssn).toEqual(undefined)
+    })
+    it('age should be 23', () => {
         expect(person.age).toEqual(23)
     })
 })
@@ -24,8 +32,10 @@ describe('should be able access array item count', () => {
     let arr2 = [1,2,3]
     let arrItemCount = arr.length
     let arr2ItemCount = arr2.length
-    it('should get correct array item counts', () => {
+    it('should get correct item counts for arr', () => {
         expect(arrItemCount).toEqual(1)
+    })
+    it('should get correct item counts for arr2', () => {
         expect(arr2ItemCount).toEqual(3)
     })
 })
--- tests/data-access-non-primitives.js
+++ tests/data-access-non-primitives.js
@@ -8,11 +8,19 @@ describe('should be able to get/delete/modify values from objects', () => {
     let first
     let last
     let full
-    it('should access person properly', () => {
+    it('should access persons first name', () => {
         expect(first).toEqual('John')
+    })
+    it('should access persons last name', () => {
         expect(last).toEqual('Smith')
+    })
+    it('should access persons full name', () => {
         expect(full).toEqual('John Smith')
+    })
+    it('should not have a ssn', () => {
         expect(person.ssn).toEqual(undefined)
+    })
+    it('age should be 23', () => {
         expect(person.age).toEqual(23)
     })
 })
@@ -22,8 +30,10 @@ describe('should be able access array item count', () => {
     let arr2 = [1,2,3]
     let arrItemCount
     let arr2ItemCount
-    it('should get correct array item counts', () => {
+    it('should get correct item counts for arr', () => {
         expect(arrItemCount).toEqual(1)
+    })
+    it('should get correct item counts for arr2', () => {
         expect(arr2ItemCount).toEqual(3)
     })
 })
