f5213ddb2a7ef79187944e7fe32da85ba3b469f4
thomas1117 <thomasshannon117@yahoo.com>
2020-08-07 12:25:47 -0700

    es6

--- solutions/es6.js
+++ solutions/es6.js
@@ -1,5 +1,28 @@
-describe('should be a placeholder', () => {
-    it('should be a placeholder', () => {
-        expect(true).toEqual(true)
+// destructuring (arrays and objects)
+// rest operator
+// spread operator
+describe('should know destructuring of objects', () => {
+    const person = {name: 'John', age: 55, skills: ['archery']}
+    it('should destructure person name', () => {
+        expect(name).toEqual('John')
+    })
+    it('should destructure person age', () => {
+        expect(age).toEqual(55)
+    })
+    it('should destructure person skills', () => {
+        expect(skills).toEqual(['archery'])
+    })
+})
+
+describe('should know destructuring of arrays', () => {
+    const inventory = ['longsword', 'apple', 'bow and arrow']
+    it('should be a longsword', () => {
+        expect(sword).toEqual('longsword')
+    })
+    it('should be a apple', () => {
+        expect(food).toEqual('apple')
+    })
+    it('should be a bow and arrow', () => {
+        expect(bow).toEqual('bow and arrow')
     })
 })
